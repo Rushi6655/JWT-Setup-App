c0adf558e615e02d95eb8d44cb0ffc7025f02cb2
Hrushikesh Shelke <h.shelke@rnt.ai>
2023-04-17 14:36:01 +0530

    rnt prod setup

---
+++ src/main/java/com/mainapp/mainapp/Controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.mainapp.mainapp.Controller;
+
+import com.mainapp.mainapp.Entity.RequestObj;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String getHome(){
+        return "Home Page";
+    }
+    @PostMapping("/addUser")
+    public ResponseEntity<RequestObj> addUser(@RequestBody RequestObj user){
+        return new ResponseEntity<>(user, HttpStatus.FOUND);
+    }
+}
